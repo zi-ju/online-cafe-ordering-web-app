6e953e151ce2659e77bd6d72dd0820b1f3393e22
Zi <juzi@ZIdeMacBook-Pro.local>
2024-06-26 20:18:18 -0700

    Modified for deployment.

--- api/index.js
+++ api/index.js
@@ -418,6 +418,11 @@ app.post("/verify-user", requireAuth, async (req, res) => {
 });
 
 
-app.listen(8000, () => {
-  console.log("Server running on http://localhost:8000 🎉 🚀");
+// app.listen(8000, () => {
+//   console.log("Server running on http://localhost:8000 🎉 🚀");
+// });
+
+const PORT = parseInt(process.env.PORT) || 8080;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT} 🎉 🚀`);
 });
